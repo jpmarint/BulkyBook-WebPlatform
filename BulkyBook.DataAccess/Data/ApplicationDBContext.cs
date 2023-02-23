3f9c71da1ffcb9758b41879f4271b6159ec63607
Juan Pablo Marin <a01702248@itesm.mx>
2023-02-23 15:31:00 -0500

    Add files via upload

---
+++ BulkyBook.DataAccess/Data/ApplicationDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace BulkyBook.DataAccess
+{
+    public class ApplicationDBContext : IdentityDbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CoverType> CoverTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
